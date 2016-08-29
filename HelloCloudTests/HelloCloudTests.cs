aea5d56e34ddc82459b6936dfce9d2417bba8875
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2016-08-29 02:25:08 +0000

    Adding new project hellocloud developed in csharp

---
+++ HelloCloudTests/HelloCloudTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloCloud;
+
+namespace HelloCloudTests
+{
+    [TestClass]
+    public class HelloCloudTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello Cloud", Program.CreateMessage());
+        }
+    }
+}
